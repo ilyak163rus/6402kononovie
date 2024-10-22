c5fd086733df24e1db92713c837964ee72f0607d
Ilya <ilyakononovv@mail.ru>
2024-10-22 12:40:58 +0400

    Исправления

--- main.py
+++ main.py
@@ -23,58 +23,58 @@ def config_generate():
         file.write("b = " + str(b) + '\n')
         file.write("c = " + str(c))
 
-def read_config(filename):
+def read_config(filename: str) -> dict:
     """
     Функция для чтения конфигурационного файла. 
-    Принимает имя файла и возвращает кортеж значений.
+    Принимает имя файла и возвращает словарь значений.
 
     Параметры:
     filename (str): Имя файла для чтения.
 
     Возвращает:
-    tuple: Кортеж значений из конфигурационного файла.
+    dict: Словарь значений из конфигурационного файла.
     """
+    config = {}
     with open(filename, 'r') as f:
         lines = f.readlines()
 
-    values = []
     for line in lines:
         key, value = line.strip().split(' = ')
-        values.append(int(value))
+        config[key] = int(value)
 
-    return tuple(values)
+    return config
 
-def calculate_and_save(n0, h, nk, a, b, c):
+def calculate_and_save(n0:float, h:float, nk:float, a:float, b:float, c:float):
     """
     Функция для вычисления значения функции y = a / (1 + np.exp(-b * x + c)) 
     для заданного диапазона значений x и заданных параметров a, b, c.
     Записывает результаты в файл 'results.txt'.
 
     Параметры:
-    n0 (int): Начальное значение диапазона x.
-    h (int): Шаг изменения x.
-    nk (int): Конечное значение диапазона x.
-    a (int): Параметр a из конфигурации.
-    b (int): Параметр b из конфигурации.
-    c (int): Параметр c из конфигурации.
+    n0 (float): Начальное значение диапазона x.
+    h (float): Шаг изменения x.
+    nk (float): Конечное значение диапазона x.
+    a (float): Параметр a из конфигурации.
+    b (float): Параметр b из конфигурации.
+    c (float): Параметр c из конфигурации.
     """
-    x = np.arange(n0, nk, h)  # создаем массив значений x с заданным шагом
-    y = a / (1 + np.exp(-b * x + c))  # вычисляем значение функции y для каждого x
+    x = np.arange(n0, nk, h)  
+    y = a / (1 + np.exp(-b * x + c))  
 
     with open('results.txt', 'w') as f:
         for i in range(len(x)):
-            f.write(f'x={x[i]}, y={y[i]}\n')  # записываем результаты в файл
+            f.write(f'x={x[i]}, y={y[i]}\n')  
 
 def main():
     """Основная функция программы."""
     # Создаем парсер
     parser = argparse.ArgumentParser(description='Обработка параметров функции.')
-    parser.add_argument('--n0', type=int, help='Начальное значение диапазона x.')
-    parser.add_argument('--h', type=int, help='Шаг изменения x.')
-    parser.add_argument('--nk', type=int, help='Конечное значение диапазона x.')
-    parser.add_argument('--a', type=int, help='Параметр a из конфигурации.')
-    parser.add_argument('--b', type=int, help='Параметр b из конфигурации.')
-    parser.add_argument('--c', type=int, help='Параметр c из конфигурации.')
+    parser.add_argument('--n0', type=float, help='Начальное значение диапазона x.')
+    parser.add_argument('--h', type=float, help='Шаг изменения x.')
+    parser.add_argument('--nk', type=float, help='Конечное значение диапазона x.')
+    parser.add_argument('--a', type=float, help='Параметр a из конфигурации.')
+    parser.add_argument('--b', type=float, help='Параметр b из конфигурации.')
+    parser.add_argument('--c', type=float, help='Параметр c из конфигурации.')
 
     # Парсим аргументы
     args = parser.parse_args()
@@ -83,8 +83,15 @@ def main():
         calculate_and_save(args.n0, args.h, args.nk, args.a, args.b, args.c)
     else:
         config_generate()
-        n0, h, nk, a, b, c = read_config('config.txt')
+        config = read_config('config.txt')
+        n0 = config['n0']
+        h = config['h']
+        nk = config['nk']
+        a = config['a']
+        b = config['b']
+        c = config['c']
         calculate_and_save(n0, h, nk, a, b, c)
 
 if __name__ == "__main__":
     main()
+
