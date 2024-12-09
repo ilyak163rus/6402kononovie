081abb63b6f1d493077ec87a5df4358f291b8291
ilyak163rus <97436959+ilyak163rus@users.noreply.github.com>
2024-12-10 00:55:29 +0400

    Delete data_analysis directory

--- data_analysis/data_fetcher/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Модуль data_fetcher для получения данных из Google Trends.
-"""
-
-from .fetcher import DataFetcher
-
-__all__ = ["DataFetcher"]
--- data_analysis/realtime_service/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Модуль realtime_service для отслеживания изменений данных в реальном времени.
-"""
-
-from .service import RealTimeService
-
-__all__ = ["RealTimeService"]
